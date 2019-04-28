1dec95c32f406423b4507d25758eb39f0ff2cf15
Uniden <kakuninhuka@gmail.com>
2019-04-29 03:42:31 +0900

    外観のテーマを黒色に修正

--- BrainfuckIDE/MainWindow.xaml.cs
+++ BrainfuckIDE/MainWindow.xaml.cs
@@ -3,9 +3,11 @@ using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -24,12 +27,63 @@ namespace BrainfuckIDE
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
 
         }
+
+        private void Caption_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        public bool IsNotMaximize
+            => this.WindowState != WindowState.Maximized;
+        public bool IsNotNomal
+            => this.WindowState != WindowState.Normal;
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            RaiseNotifyPropertyChanged(nameof(IsNotMaximize));
+            RaiseNotifyPropertyChanged(nameof(IsNotNomal));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected void RaiseNotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
+
 }
