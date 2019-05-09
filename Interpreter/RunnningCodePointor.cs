bfa2ef3be8772b2b6ab6426dec1b91cb52ff831e
Uniden <kakuninhuka@gmail.com>
2019-05-09 13:14:51 +0900

    名前空間などを変更

--- Interpreter/RunnningCodePointor.cs
+++ Interpreter/RunnningCodePointor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BrainfuckInterpreter
+namespace Interpreter
 {
 
     /// <summary> ソースコードの実行ポインタを管理するクラス </summary>
