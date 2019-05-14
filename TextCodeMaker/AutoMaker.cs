7d6c818a69ae0ad1572b113fbff03ffc8847eefa
Uniden <kakuninhuka@gmail.com>
2019-05-14 16:47:35 +0900

    Addし忘れの追加と　README.md の追記

--- TextCodeMaker/AutoMaker.cs
+++ TextCodeMaker/AutoMaker.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CsUtility.Primitive;
+using CsUtility.Enumerable;
 namespace TextCodeMaker
 {
-    class AutoMaker
+    public static class AutoMaker
     {
         /// <summary>
         /// 末尾に\n\0を追加して、その文字の文字コードをメモリに格納するコードを生成する
@@ -47,12 +48,58 @@ namespace TextCodeMaker
 
         static private string RepeatString(string str, int count)
         {
-            var charArray = str.ToCharArray();
-            return new string(Enumerable.Repeat(charArray, count).SelectMany(p => p).ToArray());
+            return new string(Enumerable.Repeat(str, count).SelectMany(p => p).ToArray());
         }
         static private string RepeatString(char letter, int count)
         {
             return new string(Enumerable.Repeat(letter, count).ToArray());
         }
+
+        static public string GetSimplyTextOutputer(string text)
+        {
+            const int baseDivider = 10;
+
+            if (text.IsNullOrEmpty()) return string.Empty;
+            var baseChars =
+                text.AsEnumerable()
+                .ZipVicinity((p, n) => (New: n, Deff: Math.Abs(n - p)))
+                .Where(p => p.Deff >= 20).Select(p=>p.New)
+                .Append(text[0])
+                .Select(p => (p + (baseDivider / 2)) / baseDivider * baseDivider).ToArray();
+
+            var retStrs = $"++++++++++[->"
+                + string.Join(">", baseChars.Select(p => RepeatString('+', p/10)))
+                + RepeatString('<', baseChars.Length)
+                + "]>";
+
+            IEnumerable<string> Itr()
+            {
+                var pos = 0;
+                foreach (var item in text)
+                {
+                    int newPos = GetMinimusIndexes(baseChars, p => Math.Abs(p - item));
+                    if (newPos > pos) yield return RepeatString('>', newPos - pos);
+                    else if (newPos < pos) yield return RepeatString('<', pos - newPos);
+                    pos = newPos;
+
+                    if (item > baseChars[pos]) yield return RepeatString('+', item - baseChars[pos]);
+                    else if (item < baseChars[pos]) yield return RepeatString('-', baseChars[pos] - item);
+                    baseChars[pos] = item;
+
+                    yield return ".";
+                }
+                yield return "[>]";
+            }
+
+
+            return retStrs + string.Join(string.Empty, Itr());
+
+        }
+
+        private static int GetMinimusIndexes<T>(IEnumerable<T> source, Func<T, int> resSelectrt)
+        {
+            return source.Select((p, i) => (I: i, V: p)).MinItem(p => resSelectrt(p.V)).I;
+        }
+
     }
 }
